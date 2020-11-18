6706626f4352e6cead76693d44f2320125e8aaa4
Jairaj Ramkissoon <jairaj@live.com>
2020-11-18 01:23:54 -0400

    Converted to Flask

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+import os
+from flask import Flask, request, render_template
+import requests
 
-app = FastAPI()
+app = Flask(__name__)
 
 
-@app.post('/webhook/',  status_code=200)
-async def index(messages: dict):
-    print (messages)
-    return messages
+@app.route('/webhook', methods=['POST'])
+def handle_form():
+    data = request.json
+    print (data)
+    return data
+
+
+@app.route("/")
+def index():
+    return render_template("index.html");   
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)

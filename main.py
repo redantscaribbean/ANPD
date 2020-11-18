0daa342ab356463339f7a7aecd3b8c61eceda20e
Jairaj Ramkissoon <jairaj@live.com>
2020-11-18 01:31:17 -0400

    Converted to Flask

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def handle_form():
+    request.headers["Content-Type"] = "multipart/form-data; charset=utf-8"
     data = request.json
     print (data)
     return data

9e439bc77eb1d15ad6d4ba17b5e84551999840a2
Jairaj Ramkissoon <jairaj@live.com>
2020-11-21 23:40:16 -0400

    added Socket Io

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import requests
 import pymongo
 import Models.LicensePlate as LicensePlate
 import json
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-
+socketio = SocketIO(app, cors_allowed_origins='*')
+app.config['SECRET_KEY'] = 'secret!'
 
 client = pymongo.MongoClient("mongodb+srv://redants:disruptiveCaribbean@whtatsappdev.e5gab.azure.mongodb.net/ANPD?retryWrites=true&w=majority")
 db = client["ANPD"]
 collection = db["Plates"]
 LicensePlate = LicensePlate.LicensePlate()
-
 
 @app.route('/webhook', methods=['POST'])
 def handle_form():
@@ -23,13 +24,19 @@ def handle_form():
     LicensePlate.plate = plate
     print (plate)
     collection.insert_one(LicensePlate.dict())
+    socketio.send(plate, broadcast=True)
     return "Ok"
 
 
+"""@socketio.on('message')
+def send_message(plate):
+    print('------' + plate)
+    send(plate, broadcast=True)"""
+
 @app.route("/")
 def index():
-    return render_template("index.html");   
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    socketio.run(app)
